a8522a41f357782a72a89c0bdc71fbe955b2d208
Gametypi <till1brinkmann@gmail.com>
2020-06-12 21:49:54 +0200

    update structure. add header file

--- csrc/Vulkantest.cpp
+++ csrc/Vulkantest.cpp
@@ -2,13 +2,13 @@
 #include "GLFW/glfw3.h"
 #include <iostream>
 
-int main (int argc, char *argv[]) {
-  if (!glfwInit()) {
-    std::cout << "GLFW init failed";
-    return -1;
-  }
+#include "Vulkantest.h"
 
+int main (int argc, char *argv[]) {
+  glfw_init();
   GLFWwindow *window = glfwCreateWindow(1280, 720, "Vulkantest", NULL, NULL);
+
+  vulkan_init();
 
   while (!glfwWindowShouldClose(window))
   {
@@ -18,4 +18,15 @@ int main (int argc, char *argv[]) {
   glfwDestroyWindow(window);
 
   return 0;
+}
+
+void glfw_init() {
+  if (!glfwInit()) {
+    std::cout << "GLFW init failed";
+    exit(-1);
+  }
+}
+
+void vulkan_init() {
+
 }
